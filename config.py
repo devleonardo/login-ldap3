d13e4fe4c3493c88712d049df35cb1fb8b2b73cd
Leonardo Martins <leonardomartins.ads@gmail.com>
2023-05-08 14:14:47 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
-LDAP_HOST = 'ldap://192.168.101.243:389'
-LDAP_BASE_DN = 'dc=intranet,dc=leste'
-LDAP_USER_DN = 'glpi2'
-LDAP_USER_PASSWORD = 'Leste@2023'
-LDAP_GROUP_DN = 'CN=USERS_STI,OU=SISTEMASTI,DC=intranet,DC=leste'
+LDAP_HOST = 'ldap://localhost:389'
+LDAP_BASE_DN = 'dc=exemplo,dc=com'
+LDAP_USER_DN = 'admin'
+LDAP_USER_PASSWORD = 'senhaAdmin'
+LDAP_GROUP_DN = 'CN=GRUPO,OU=UNIDADE_ORGANIZADORA,DC=exemplo,DC=com'
