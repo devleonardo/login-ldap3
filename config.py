3d5a51b18faeed77d24035ce258bd68740a25e3b
Leonardo Martins <leonardomartins.ads@gmail.com>
2023-05-08 14:22:09 -0300

    informações sensíveis

--- config.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-LDAP_HOST = 'ldap://localhost:389'
-LDAP_BASE_DN = 'dc=exemplo,dc=com'
-LDAP_USER_DN = 'admin'
-LDAP_USER_PASSWORD = 'senhaAdmin'
-LDAP_GROUP_DN = 'CN=GRUPO,OU=UNIDADE_ORGANIZADORA,DC=exemplo,DC=com'
